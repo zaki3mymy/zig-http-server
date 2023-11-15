f54758ea793cec5b083bcefb745997e7d927d566
zaki3mymy <zaki.last@gmail.com>
2023-11-15 14:19:43 +0000

    chore: wip

--- src/main.zig
+++ src/main.zig
@@ -1,14 +1,25 @@
 const std = @import("std");
 const http = std.http;
+const log = std.log;
 
 const datetime = @import("./util/datetime.zig");
 const logging = @import("./util/logger.zig").logging;
+const files = @import("./files.zig");
 
 pub fn main() !void {
     var server = http.Server.init(std.heap.page_allocator, .{ .reuse_address = true });
     defer server.deinit();
 
     try server.listen(try std.net.Address.parseIp("127.0.0.1", 8080));
+
+    var out_buffer: [std.fs.MAX_PATH_BYTES]u8 = undefined;
+    const absPath = try std.fs.realpath("./html", &out_buffer);
+    log.info("createFileSet...", .{});
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    const fileSet = try files.createFileSet(absPath, allocator);
+    _ = fileSet;
+    log.info("createFileSet has done.", .{});
 
     while (true) {
         const acceptOptions = .{ .allocator = std.heap.page_allocator, .header_strategy = .{ .dynamic = 8192 } };
@@ -32,8 +43,10 @@ fn handler(res: *http.Server.Response) !void {
     //         return;
     //     }
     // };
-    if (res.wait()) |value| {
-        logging(value);
+    log.info("request from {}", .{res.address});
+
+    if (res.wait()) {
+        // logging(req.target);
     } else |err| switch (err) {
         http.Server.Request.ParseError.UnknownHttpMethod => {
             res.status = http.Status.method_not_allowed;
